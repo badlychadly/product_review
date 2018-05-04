9ed5121001320c82661b335c3fc596ef45c63b10
badlychadly <chadmontoya21@yahoo.com>
2018-05-03 20:54:34 -0400

    remove config/storage.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'static/welcome'
-  resources :users
+  root 'static#welcome'
+
+
+  resources :users, except: [:new, :create]
+  get 'signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
