d1935113525b1d487570e18f04d8d46552bcb8c2
badlychadly <chadmontoya21@yahoo.com>
2018-05-04 15:43:50 -0400

    write users feature spec

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "Users", type: :feature do
+  describe "new user signup" do
+    it "creates a new user" do 
+      visit new_user_path 
+      fill_in "user[username]", with: "Mark"
+      fill_in "user[email]", with: "mark@gmail.com"
+      fill_in "user[password]", with: "markmark"
+      fill_in "user[password_confirmation]", with: "markmark"
+      click_button "Sign up"
+      expect(User.find_by(username: "Mark").username).to eq "Mark"
+    end
+  end
+
+end
