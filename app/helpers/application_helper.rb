5f0c9c376dde0247fe11cb305d1ee058f54cc5e5
badlychadly <chadmontoya21@yahoo.com>
2018-05-09 16:38:26 -0400

    refactor navbar, add #nav_content to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+
+  def nav_content
+    if logged_in?
+      drop_down_nav
+    else
+      login_nav_link + signup_nav_link
+    end
+  end
   
   def login_nav_link
     content_tag(:li, link_to("Login", login_path, class: "nav-link"), class: "nav-item")
@@ -8,32 +16,8 @@ module ApplicationHelper
     content_tag(:li, link_to("Signup", signup_path, class: "nav-link"), class: "nav-item")
   end
 
-  # def drop_down_nav
-  #   content_tag(
-  #     :li, content_tag(
-  #       :a, class: "nav-link dropdown-toggle",
-  #        'href'=>"#",
-  #         id: "navbarDropdownMenuLink",
-  #        'data-toggle'=>"dropdown",
-  #        'aria-haspopup'=>"true",
-  #        'aria-expanded'=>"false"),
-  #        content_tag(:div,
-  #         link_to("Action", "#", class: "dropdown-menu"),
-  #         link_to("Another", "#", class: "dropdown-menu"),
-  #         link_to("Somethin", "#", class: "dropdown-menu")
-  #         class: "nav-link dropdown-toggle", 'aria-labelledby'=>"navbarDropdownMenuLink"),
-  #           class: "nav-item dropdown")
-
-    # <li class="nav-item dropdown">
-    #       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    #         Dropdown link
-    #       </a>
-    #       <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-    #         <a class="dropdown-item" href="#">Action</a>
-    #         <a class="dropdown-item" href="#">Another action</a>
-    #         <a class="dropdown-item" href="#">Something else here</a>
-    #       </div>
-    #     </li>
-  # end 
+  def drop_down_nav
+    render  "layouts/nav/dropdown"
+  end 
 
 end
