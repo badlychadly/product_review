d6266252ab7d902aab5a5d174723ea51844f9fa8
badlychadly <chadmontoya21@yahoo.com>
2018-05-09 20:53:59 -0400

    add product reviews to user/show

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -4,4 +4,13 @@ module ReviewsHelper
     link_to "Edit Review", edit_product_review_path(@product, review) if review.user == current_user 
   end
 
+  def figcaption_for_reviews(product)
+    product.reviews.each do |r|
+      content_tag(:figcaption, r.content, class: "figure-caption")
+    end
+    # product.reviews_content.each do |content|
+    #   content_tag(:figcaption, content, class: "figure-caption")
+    # end
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
+
+  def reviews_content
+    reviews.pluck(:content)
+  end
 end
