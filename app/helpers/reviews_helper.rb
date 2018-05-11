4921753d7c39514a7b315a8635dd7e7eb109b0fd
badlychadly <chadmontoya21@yahoo.com>
2018-05-11 15:51:29 -0400

    move shared partials into application folder

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -4,12 +4,6 @@ module ReviewsHelper
     link_to "Edit Review", edit_product_review_path(review.product, review), class: "card-link" if review.user == current_user 
   end
 
-  def figcaption_for_reviews(review)
-    content_tag :figcaption, review , class: "figure-caption" do
-      yield
-    end
-  end
-
   def div_card_for_review(review)
     content_tag :div, review, class: "card" do
       yield
