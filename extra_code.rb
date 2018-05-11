90d20d988f21ca95fa8cd17fbdb7ba68054e5c8f
badlychadly <chadmontoya21@yahoo.com>
2018-05-11 15:01:27 -0400

    test vb

--- extra_code.rb
+++ extra_code.rb
@@ -1,13 +1,4 @@
-<div class="col-sm-3">
-  
-    <figure class="figure bg-light">
-      <strong><%= product.name %></strong>
-      <h4></h4>
-      <img src="<%= product.img %>" class="figure-img img-fluid rounded w-100" alt="A generic square placeholder image with rounded corners in a figure.">
-      <%= render collection: product.reviews, partial: "reviews/figcaption", as: :review %>
-      <%= render partial: "products/collapse_description", locals: {product: product} %>
-    </figure>
-  </div>
+
 
 
   <%= modal_for_reviews(review) do %>
