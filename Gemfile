b004ed810b380b50a9071917ffd0b0063c9be3b3
badlychadly <chadmontoya21@yahoo.com>
2018-05-18 17:05:45 -0400

    add dotenv-rails gem+omniauth+amazon-omniauth, add .env to gitignore, add omniauth.rb for amazon authentication

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'sqlite3'
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+gem 'omniauth'
+gem 'omniauth-amazon'
+gem 'dotenv-rails'
 
 gem 'bootstrap', '~> 4.0.0'
 gem 'jquery-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
+  get '/auth/amazon/callback' => 'sessions#create'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
