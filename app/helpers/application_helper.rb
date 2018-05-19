06a513b6f46ae9c520d6fe0ca34d0d38c262cade
badlychadly <chadmontoya21@yahoo.com>
2018-05-18 23:12:27 -0400

    add login with amazon button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     if logged_in?
       drop_down_nav
     else
-      login_nav_link + signup_nav_link
+      amazon_img_login_link + login_nav_link + signup_nav_link
     end
   end
   
@@ -19,6 +19,12 @@ module ApplicationHelper
   def drop_down_nav
     render  "layouts/nav/dropdown"
   end 
+
+  def amazon_img_login_link
+    link_to('/auth/amazon') do
+      image_tag("https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png", alt: "Login with Amazon", class: "nav-link")
+    end 
+  end
 
   def flash_message(flash)
     if flash
