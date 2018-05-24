78a81ed88b8889646337d4f021fd77039a7c77ff
badlychadly <chadmontoya21@yahoo.com>
2018-05-24 18:56:33 -0400

    test vb

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   
 
 
-  resources :users, except: [:new, :create]
+  resources :users except: [:new, :create]
    
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
