fa8c768c05f074c0e2f032e4d8c8dc8e6161c97e
badlychadly <chadmontoya21@yahoo.com>
2018-05-24 23:47:28 -0400

    add numericality price validation for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
   acts_as_votable
   validates :name, presence: true, uniqueness: true
   validates :description, :link, :price, :img, presence: true
+  validates :price, numericality: true
 
   scope :most_reviews, -> {joins(:reviews).group(:name).order(Arel.sql("COUNT(reviews.id) DESC"))}
   scope :best_voted, -> {order(cached_votes_up: :desc)}
