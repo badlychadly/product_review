c9a5119862fd3a0406cfc6f6da74e771d3da8253
badlychadly <chadmontoya21@yahoo.com>
2018-05-28 16:55:48 -0400

    add field_with_errors to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
 
   def create
     if auth
-      user = User.find_or_create_omniauth(auth)
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+      @user = User.find_or_create_omniauth(auth)
+      session[:user_id] = @user.id
+      redirect_to products_path
     else
-      user = User.find_by(email: params[:session][:email])
-      if user.try(:authenticate, params[:session][:password])
-        session[:user_id] = user.id
+      @user = User.find_by(email: params[:session][:email])
+      if @user.try(:authenticate, params[:session][:password])
+        session[:user_id] = @user.id
         redirect_to products_path
       else
         flash[:alert] = "Invalid Email/Password"
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,15 @@
 module SessionsHelper
+
+  def div_for_errors(flash)
+    if !!flash 
+      content_tag :div, class: "field_with_errors" do 
+        # content_tag(:small, "#{flash}", class: "form-text text-danger")
+          # <small class="form-text text-danger"><%= flash[:alert] %></small>
+        
+        yield
+      end 
+    else 
+      yield
+    end
+  end
 end
