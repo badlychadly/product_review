6257db6c6ebeb16a99034563f8b4bda1a5ca228d
badlychadly <chadmontoya21@yahoo.com>
2018-05-29 21:20:05 -0400

    add to notes

--- notes.rb
+++ notes.rb
@@ -1,6 +1,11 @@
 
 
 
-# do something about reviews form on reviews/index for new review
 
 #  how do I handle routing error when an unsuccessful form request renders the page, and if I hit refresh a get request is made for a route that does not have a get defined??????    WHEN UPDATING A REVIEW
+
+# how to change numerical validation input before validation? ex price on product
+
+#how to dry up all of the redirects in my controllers?
+
+# how do I avoid making changes on all of my views, when they all need different content, like how I use my header helper method on every view
