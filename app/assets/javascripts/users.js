7b0317da92e644343072c0c333ae05d6d530c9cc
badlychadly <chadmontoya21@yahoo.com>
2018-07-30 19:42:47 -0700

    add review_serializer

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,10 @@
 $(function() {
     $('.load_reviews').on('click', function (event) {
         $.get(this.href + '.json', function (json) {
-            debugger;
+            // debugger;
+            json.reviews.forEach(function (review) {
+                $('#insert_reviews').append(`<p>${review.content}</p>`)
+            })
         })
         event.preventDefault()
     })    
