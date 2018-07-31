6a797e06358377b7697b3fde643d6864e7b88005
badlychadly <chadmontoya21@yahoo.com>
2018-07-31 12:55:36 -0700

    add reviews_list.hbs template to users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,9 +2,13 @@ $(function() {
     $('.load_reviews').on('click', function (event) {
         $.get(this.href + '.json', function (json) {
             // debugger;
-            json.reviews.forEach(function (review) {
-                $('#insert_reviews').append(`<p>${review.content}</p>`)
-            })
+            // json.reviews.forEach(function (review) {
+            //     $('#insert_reviews').append(`<p>${review.content}</p>`)
+            // })
+            reviewsHtml = HandlebarsTemplates['reviews_list']({
+                reviews: json.reviews
+            });
+            $('#insert_reviews').append(reviewsHtml)
         })
         event.preventDefault()
     })    
