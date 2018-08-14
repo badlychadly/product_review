9b17ab20f23fa117e37fa120c4f4b2102175af50
badlychadly <chadmontoya21@yahoo.com>
2018-08-13 20:01:03 -0700

    add es6 class to users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,46 +1,55 @@
 
-function User(attributes) {
-    this.id = attributes.id
-    this.username = attributes.username
-    this.email = attributes.email
-    this.reviews = attributes.reviews
-}
-
-
-User.prototype.createReviews = function (reviews) {
-    return this.reviews = reviews.map(review => new Review(review))
-}
-
-User.createUser = function (json) {
-    var user = new User(json)
-    user.createReviews(user.reviews)
-    return user
-}
-
-
-User.loadReviews = function (event) {
-    if ($('#reviewsList').children().is('li')) {
-        $('#reviewsList').remove()
-    } else {
-        $.get(this.href + '.json', function (json) {
-            var user = User.createUser(json)
-            reviewsHtml = HandlebarsTemplates['reviews_list']({
-                reviews: user.reviews
-            });
-            $('#insert_reviews').append(reviewsHtml)
-        })
+class User {
+    constructor(attributes) {
+        this.id = attributes.id
+        this.username = attributes.username
+        this.email = attributes.email
+        this.reviews = attributes.reviews
     }
-    event.preventDefault()
+
+
+    createReviews(reviews) {
+        return this.reviews = reviews.map(review => new Review(review))
+    }
+
+    renderList() {
+        return User.template(this)
+    }
+
+    
+    static createUser(json) {
+        var user = new User(json)
+        user.createReviews(user.reviews)
+        return user
+    }
+    
+    
+    static loadReviews(event) {
+        if ($('#reviewsList').children().is('li')) {
+            $('#reviewsList').remove()
+        } else {
+            $.get(this.href + '.json', function (json) {
+                var user = User.createUser(json)
+                debugger;
+                // reviewsHtml = HandlebarsTemplates['reviews_list'];
+                var reviewsList = user.renderList()
+                // $('#insert_reviews').append(reviewsList)
+            })
+        }
+        event.preventDefault()
+    }
+    
+    static loadReviewsListener() {
+        $(document).on('click', '.load_reviews', User.loadReviews)
+    }
+
 }
 
-User.loadReviewsListener = function () {
-    $(document).on('click', '.load_reviews', User.loadReviews)
-}
+
 
 $(function () {
+    // var ast = Handlebars.parse('reviews_list')
+    // Handlebars.precompile(ast)
+    User.template = HandlebarsTemplates['reviews_list']
     User.loadReviewsListener()
 })
-
-// $(document).on('turbolinks:load', function () {
-//       User.loadReviewsListener()  
-// })
